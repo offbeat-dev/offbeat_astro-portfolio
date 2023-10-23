78309df8f810ad2ade98890b948a9243d22c740d
offbeat-dev <offbeat.it.all@gmail.com>
2023-10-23 12:48:08 -0500

    :tada: - add vercel analytics

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,15 @@
 import { defineConfig } from "astro/config";
 import Icons from "unplugin-icons/vite";
+import vercel from "@astrojs/vercel/serverless";
 
 // https://astro.build/config
 export default defineConfig({
+  output: "server",
+  adapter: vercel({
+    webAnalytics: {
+      enabled: true,
+    },
+  }),
   vite: {
     plugins: [
       Icons({
